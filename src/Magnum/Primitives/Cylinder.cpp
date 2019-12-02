c41b2e7a887c81cdb51eb0d007d2f94d14ead02e
Vladimír Vondruš <mosra@centrum.cz>
2019-12-02 01:38:23 +0100

    Primitives: update to use the new MeshData.
    The internals don't use any std::vector anymore, only the icosphere
    needs an std::unordered_map to do duplicate removal. Additionally, the
    most simple primitives are now simply views on constant data,
    being completely zero-allocation.
    
    On a Mac this resulted in the dylib going down from 1.5 MB to 418 kB in
    Debug, and from 129 kB to 90 kB in Release. Quite nice.
    
    The tests are not ported away from MeshDataXD yet as I want to ensure
    the behavior is *exactly* as before.

--- src/Magnum/Primitives/Cylinder.cpp
+++ src/Magnum/Primitives/Cylinder.cpp
@@ -33,10 +33,10 @@
 
 namespace Magnum { namespace Primitives {
 
-Trade::MeshData3D cylinderSolid(const UnsignedInt rings, const UnsignedInt segments, const Float halfLength, const CylinderFlags flags) {
+Trade::MeshData cylinderSolid(const UnsignedInt rings, const UnsignedInt segments, const Float halfLength, const CylinderFlags flags) {
     CORRADE_ASSERT(rings >= 1 && segments >= 3,
         "Primitives::cylinderSolid(): at least one ring and three segments expected",
-        (Trade::MeshData3D{MeshPrimitive::Triangles, {}, {}, {}, {}, {}, nullptr}));
+        (Trade::MeshData{MeshPrimitive::Triangles, 0}));
 
     Implementation::Spheroid cylinder(segments, flags & CylinderFlag::GenerateTextureCoords ? Implementation::Spheroid::TextureCoords::Generate : Implementation::Spheroid::TextureCoords::DontGenerate);
 
@@ -71,10 +71,10 @@ Trade::MeshData3D cylinderSolid(const UnsignedInt rings, const UnsignedInt segme
     return cylinder.finalize();
 }
 
-Trade::MeshData3D cylinderWireframe(const UnsignedInt rings, const UnsignedInt segments, const Float halfLength) {
+Trade::MeshData cylinderWireframe(const UnsignedInt rings, const UnsignedInt segments, const Float halfLength) {
     CORRADE_ASSERT(rings >= 1 && segments >= 4 && segments%4 == 0,
         "Primitives::cylinderWireframe(): at least one ring and multiples of 4 segments expected",
-        (Trade::MeshData3D{MeshPrimitive::Lines, {}, {}, {}, {}, {}, nullptr}));
+        (Trade::MeshData{MeshPrimitive::Lines, 0}));
 
     Implementation::WireframeSpheroid cylinder(segments/4);
 
